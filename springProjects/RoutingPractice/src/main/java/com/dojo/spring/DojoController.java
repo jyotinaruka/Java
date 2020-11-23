99a16cc500e26c79beac286d1fa9918fadb02b78
Jyoti Singh <jn.naruka@gmail.com>
2020-11-23 14:30:43 -0800

    Routing Practice

---
+++ springProjects/RoutingPractice/src/main/java/com/dojo/spring/DojoController.java
@@ -0,0 +1,23 @@
+package com.dojo.spring;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DojoController {
+
+	@RequestMapping("/{dojo}")
+	public String displayText(@PathVariable("dojo") String dojo) {
+		if (dojo.equals("dojo")) {
+			return "The dojo is awesome!";
+		}
+		if (dojo.equals("burbank-dojo")) {
+			return "Burbank Dojo is located in Southern California";
+		}
+		if (dojo.equals("san-jose")) {
+			return "SJ dojo is the headquarters";
+		}
+		return "";
+	}
+}
---
+++ springProjects/RoutingPractice/src/main/java/com/dojo/spring/RoutingPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.dojo.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutingPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoutingPracticeApplication.class, args);
+	}
+
+}
